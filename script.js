6e8451c597f4bdea770bb9a339a7c46c8c6a4054
naymoraes <nayaramrspontes@gmail.com>
2024-04-17 14:14:30 -0400

    first login

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const mainContainer = document.querySelector('.form-signin');
+    mainContainer.classList.add('centered-container');
+  });
+  
